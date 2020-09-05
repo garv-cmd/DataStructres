9abd1d2ea392f79bdaa14c2f90ed2932b996a080
garv-cmd <54171077+garv-cmd@users.noreply.github.com>
2020-09-05 22:18:42 +0530

    Java Collection Framework | Data Structures & Algorithm

--- src/tree/Tree.java
+++ src/tree/Tree.java
@@ -6,22 +6,53 @@ public class Tree {
     static Scanner sc = null;
     public static void main(String args[]){
         sc = new Scanner(System.in);
-        createTree();
+        Node root = createTree();
+        System.out.println(root.data);
+        InOrder(root);
+        System.out.println();
+        PreOrder(root);
+        System.out.println();
+        PostOrder(root);
     }
     static Node createTree(){
         Node root = null;
-        System.out.println("Enter data : ");
+        System.out.println("Enter Data: ");
         int data = sc.nextInt();
-        if(data == -1)
+        if(data == -1){
             return null;
+        }
         root = new Node(data);
+
         System.out.println("Enter left for "+data);
         root.left = createTree();
-
         System.out.println("Enter right for "+data);
         root.right = createTree();
-
         return root;
+    }
+    static void InOrder(Node root){
+        if(root == null){
+            return;
+        }
+        InOrder(root.left);
+        System.out.print(root.data+" ");
+        InOrder(root.right);
+    }
+    static void PreOrder(Node root){
+        if(root == null){
+            return;
+        }
+        System.out.print(root.data+" ");
+        PreOrder(root.left);
+        PreOrder(root.right);
+    }
+    static void PostOrder(Node root){
+        if(root == null){
+            return;
+        }
+        PostOrder(root.left);
+
+        PostOrder(root.right);
+        System.out.print(root.data+" ");
     }
 }
 class Node{
